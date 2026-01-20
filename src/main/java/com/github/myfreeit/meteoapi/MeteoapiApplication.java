d3abd8d0aed134700019b4915d27b4efd2bb3220
MyFreeIT <MyFreeIT@outlook.com>
2026-01-20 20:57:32 +0200

    chore: initial project setup with pom, gitignore, license

---
+++ src/main/java/com/github/myfreeit/meteoapi/MeteoapiApplication.java
@@ -0,0 +1,13 @@
+package com.github.myfreeit.meteoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeteoapiApplication.class, args);
+	}
+
+}
