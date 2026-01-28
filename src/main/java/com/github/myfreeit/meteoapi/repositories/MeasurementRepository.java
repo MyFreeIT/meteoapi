31719b8d59d74a682749bf50e0968b9400813ff6
MyFreeIT <MyFreeIT@outlook.com>
2026-01-28 20:02:55 +0200

    feat(repositories): add countByRainingTrue and findBySensorName methods to MeasurementRepository

--- src/main/java/com/github/myfreeit/meteoapi/repositories/MeasurementRepository.java
+++ src/main/java/com/github/myfreeit/meteoapi/repositories/MeasurementRepository.java
@@ -4,6 +4,10 @@ import com.github.myfreeit.meteoapi.entities.Measurement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MeasurementRepository extends JpaRepository<Measurement, Long> {
+    long countByRainingTrue();
+    Optional<Measurement> findBySensorName(String name);
 }
