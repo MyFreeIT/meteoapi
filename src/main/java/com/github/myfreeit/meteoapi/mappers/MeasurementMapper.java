0dacc464215fe8a78dd4a902e464f3f698b4b094
MyFreeIT <MyFreeIT@outlook.com>
2026-01-30 02:01:37 +0200

    fix(mappers): ensure MeasurementMapper maps sensor entity to SensorDto in response

--- src/main/java/com/github/myfreeit/meteoapi/mappers/MeasurementMapper.java
+++ src/main/java/com/github/myfreeit/meteoapi/mappers/MeasurementMapper.java
@@ -3,9 +3,12 @@ package com.github.myfreeit.meteoapi.mappers;
 import com.github.myfreeit.meteoapi.dto.MeasurementDto;
 import com.github.myfreeit.meteoapi.entities.Measurement;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface MeasurementMapper {
+    @Mapping(source = "sensor", target = "sensorDto")
     MeasurementDto toDto(Measurement measurement);
+    @Mapping(source = "sensorDto", target = "sensor")
     Measurement toEntity(MeasurementDto measurementDto);
 }
